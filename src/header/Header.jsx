a950c0a8e4eed8ff3addf8060fbc566bca90d4e0
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-02 22:55:34 -0400

    added topbar css and main logo

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,7 +3,11 @@ import './header.css'
 export default function Header() {
     return (
         <div className='header'>
-            Header
+            <div className="headerTitles">
+                <span className='headerTitleSm'>React & Blog</span>
+                <span className='headerTitleLg'>Blog</span>
+            </div>
+            <img className="headerImg" src="assets/mainLogo2.png" alt="bonillaCodes logo" />
         </div>
     )
 }
