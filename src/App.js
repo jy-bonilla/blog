b19718f4dd1475995e7040841d8c85e016c83b52
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-04 17:50:20 -0400

    added singlePost component, jsx, css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/homepage/Home";
+// import Home from "./pages/homepage/Home";
+import Single from "./pages/homepage/single/Single";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Home />
+      <Single />
+      {/* <Home /> */}
     </>
 
 
