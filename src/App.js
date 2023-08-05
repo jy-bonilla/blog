30866446a012bc342209804b2e4c9fa61109db6d
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-05 14:09:57 -0400

    completed settins.css and settings.jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Topbar from "./components/topbar/Topbar";
 // import Home from "./pages/homepage/Home";
 // import Single from "./pages/homepage/single/Single";
-import Write from "./pages/write/Write"
+// import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Write />
+      {/* <Write /> */}
       {/* <Single /> */}
       {/* <Home /> */}
+      <Settings />
     </>
 
 
