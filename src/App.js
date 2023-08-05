c17707874235ef6c8516f4af43fb2749549f9663
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-05 15:53:10 -0400

    added react-router-dom and created routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import Topbar from "./components/topbar/Topbar";
-// import Home from "./pages/homepage/Home";
-// import Single from "./pages/homepage/single/Single";
-// import Write from "./pages/write/Write";
-// import Settings from "./pages/settings/Settings";
-// import Login from "./pages/login/Login.jsx";
+import Home from "./pages/homepage/Home";
+import Single from "./pages/homepage/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login.jsx";
 import Register from "./pages/register/Register";
+
+import { Route, Routes } from "react-router-dom"
+
 function App() {
-  return (
-    <>
-      <Topbar />
-      {/* <Write /> */}
-      {/* <Single /> */}
-      {/* <Home /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      <Register />
-    </>
-
-
-  );
+  const user = false;
+  return <>
+    <Topbar />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/post/:postId" element={<Single />} />
+      <Route path="/write" element={<Write />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={user ? <Home /> : <Register />} />
+    </Routes>
+  </>
 }
 
 export default App;
