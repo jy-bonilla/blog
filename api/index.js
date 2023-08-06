e0cc806a48d8fd134dd644d02a1973127c92c02e
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-05 22:07:05 -0400

    installed bcrypt

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
-dotenv.config()
+dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL,)
     .then(console.log("connected to MongoDb"))
     .catch(err => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("8000", () => {
     console.log("Backend is running");
