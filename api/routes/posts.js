77489550ffcadd4536332194ce2df21f2ea7ad50
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-07 22:06:31 -0400

    added categories route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
+const { error } = require("console");
 
 //CREATE POST
 router.post("/", async (req, res) => {
@@ -41,11 +42,13 @@ router.put("/:id", async (req, res) => {
 //DELETE POST
 router.delete("/:id", async (req, res) => {
     try {
+
         const post = await Post.findById(req.params.id);
+
         if (post.username === req.body.username) {
             try {
                 await post.delete();
-                res.status(200).json("Post has been deleted...");
+                res.status(200).json(deletePost);
             } catch (err) {
                 res.status(500).json(err);
             }
@@ -56,6 +59,7 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
 
 //GET POST
 router.get("/:id", async (req, res) => {
