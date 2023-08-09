8f8a384ca856b017bab51d44f328bba1e710250d
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-09 18:53:04 -0400

    fixed dete post route error by changing post.delete to post.deleteone

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-// const multer = require("multer");
-// const path = require("path");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 app.use(express.json());
@@ -15,6 +15,21 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL,)
     .then(console.log("connected to MongoDb"))
     .catch(err => console.log(err));
+
+const storage = multer.diskStorage(
+    {
+        destination: (req, file, cb) => {
+            cb(null, "images")
+        }, filename: (req, file, cb) => {
+            cb(null, req.body.name);
+        }
+    }
+)
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
