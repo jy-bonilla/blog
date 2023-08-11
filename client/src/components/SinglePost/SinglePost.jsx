775388968dc2329d91570497caaac98936ea6d4e
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-11 11:17:51 -0400

    added link functionality to homepage and sidebar

--- client/src/components/SinglePost/SinglePost.jsx
+++ client/src/components/SinglePost/SinglePost.jsx
@@ -1,28 +1,46 @@
-import "./singlePost.css"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./singlePost.css";
 
 export default function SinglePost() {
+    const location = useLocation();
+    const path = (location.pathname.split("/")[2]);
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get("/posts/" + path);
+            setPost(res.data)
+        };
+        getPost()
+    }, [path])
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img className="singlePostImg" src="assets/mountains.png" alt="" />
+                {post.photo && (
+                    <img className="singlePostImg" src={post.photo} alt="" />
+                )}
                 <h1 className="singlePostTitle">
-                    Lorem ipsum dolor sit amet.
+                    {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
                         <i className="singlePostIcon fa-regular fa-trash-can"></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>Bonilla</b></span>
-                    <span className="singlePosDate"> 1 hour ago</span>
+                    <span className="singlePostAuthor">
+                        Author:
+                        <Link to={`/?user=${post.username}`} className="link">
+                            <b>{post.username}</b>
+                        </Link>
+                    </span>
+                    <span className="singlePosDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
                 <p className="singlePostDesc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis facilis
-                    similique perspiciatis dolores deserunt, recusandae eum, consequatur cupiditate,
-                    optio debitis laudantium repudiandae magnam reprehenderit reiciendis quam temporibus
-                    explicabo neque asperiores?
+                    {post.desc}
                 </p>
             </div>
-        </div>
+        </div >
     )
 }
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,20 @@
-import './sidebar.css'
+import './sidebar.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
 
 export default function Sidebar() {
+    const [cats, setCats] = useState([]);
+
+    useEffect(() => {
+        const getCats = async () => {
+            const res = await axios.get("/categories")
+            setCats(res.data)
+        }
+        getCats();
+    }, [])
     return (
         <div className='sidebar'>
             <div className="sidebarItem">
@@ -13,10 +27,11 @@ export default function Sidebar() {
             <div className="sidebarItem">
                 <span className="sidebarTitle">CATEGORIES</span>
                 <ul className="sidebarList">
-                    <li className="sidebarListItem">JavaScript</li>
-                    <li className="sidebarListItem">PYTHON</li>
-                    <li className="sidebarListItem">Java</li>
-                    <li className="sidebarListItem">Tech</li>
+                    {cats.map((c) => (
+                        <Link to={`/?cat=${c.name}`} className='link'>
+                            <li className='sidebarListItem'>{c.name}</li>
+                        </Link>
+                    ))}
                 </ul>
             </div>
             <div className="sidebarItem">
