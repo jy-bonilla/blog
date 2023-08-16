2ed900b3d3b79976a24411e556c292ddb46aceb4
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-08-15 20:53:45 -0400

    npm path to api folder

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,10 +2,12 @@ import './post.css';
 import { Link } from "react-router-dom"
 
 export default function Post({ post }) {
+
+    const PF = "localhost:3000/images/"
     return (
         <div className='post'>
             {post.photo && (
-                <img className="postImg" src={post.photo}
+                <img className="postImg" src={PF + post.photo}
                     alt="misty morning over mountains" />
             )}
             <div className="postInfo">
